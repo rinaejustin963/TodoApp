c8df532e711136de0695269cd125afee293e81a7
Rinae Justin Tshivhinda <Justinrinae@gmail.com>
2022-05-25 00:05:25 +0200

    Final Product

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -8,8 +8,10 @@ import {Todo} from './../../models/Todo';
 })
 export class TodosComponent implements OnInit {
 //an Array to loop through for all Todos
- // title = "Todos";
+ // title = "Todos"; 
   todos!: Todo[];
+
+  inputTodo:string = "";
   constructor() { }
  
   ngOnInit(): void {
@@ -17,17 +19,46 @@ export class TodosComponent implements OnInit {
     this.todos = [
       {
         content: 'First Todo',
-        completed: false
+        completed: true
       },
       {
         content: 'Second Todo',
-        completed: true
+        completed: false
       },
       {
         content: 'Third Todo',
         completed: false
+      },
+      {
+        content: 'Fourth Todo',
+        completed: true
       }
     ];
   }
+//A method that will allow us to complete a todo by clicking on it
+//Here we are looping through all our Todos and checking all our Todos to see if our ID matches the id we have (id: number)
+//Here we are looping through all our Todos and checking if our int matches the id we have (id: number), if so then its gonna complete 
+toggleDone (id: number){
+    this.todos.map((v, i) =>{ 
+      if( i == id) v.completed = !v.completed;
+
+
+      return v;
+    }) 
+}
+
+deleteTodo(id: number){
+  //if i is not an id then we are not gona return it
+  this.todos = this.todos.filter((v, i) => i==id);
+}
+
+addTodo(){
+  this.todos.push({
+    content: this.inputTodo,
+    completed: false
+  });
+
+  this.inputTodo = "";
+}
 
 }
